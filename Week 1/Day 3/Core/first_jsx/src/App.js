ceefe806ecd01898d3e042696f484ab2ac44d1e1
louai60 <louaysaafi60@gmail.com>
2024-04-04 13:46:22 +0100

    MERN

---
+++ Week 1/Day 3/Core/first_jsx/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <p>* Learn React</p>
+      <p>* Climb Mt.Everest</p>
+      <p>* Run a Marathon</p>
+      <p>* Feed the dogs</p>
+    </div>
+  );
+}
+
+export default App;
