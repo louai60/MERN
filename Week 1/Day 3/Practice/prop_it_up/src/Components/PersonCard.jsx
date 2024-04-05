13d92b1fe239d75b546b508362bd1ecbddd703da
louai60 <louaysaafi60@gmail.com>
2024-04-05 13:00:10 +0100

    MERN

--- Week 1/Day 3/Practice/prop_it_up/src/Components/PersonCard.jsx
+++ Week 1/Day 3/Practice/prop_it_up/src/Components/PersonCard.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const PersonCard = (props) => (
-    <div>
+const PersonCard = (props) => {
+    const [age, setAge] = useState(props.age);
+
+    const IncreamentAge = () => {
+        setAge(age + 1);
+    };
+
+    return (
         <div>
-            <h1>{props.lastName}, {props.firstName}</h1>
-            <p>Age: {props.age}</p>
-            <p>Hair Color: {props.hairColor}</p>
+            <div>
+                <h1>{props.lastName}, {props.firstName}</h1>
+                <p>Age: {age}</p>
+                <p>Hair Color: {props.hairColor}</p>
+            </div>
+            <button onClick={IncreamentAge}>Birthday Button for {props.firstName} {props.lastName}</button>
         </div>
-    </div>
-);
+    );
+};
 
 export default PersonCard;
+
