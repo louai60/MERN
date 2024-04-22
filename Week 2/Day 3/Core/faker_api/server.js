8421e732b573da2877374db1a18dcb2cc963a4db
louai60 <louaysaafi60@gmail.com>
2024-04-22 13:42:39 +0100

    MERN

--- Week 2/Day 3/Core/faker_api/server.js
+++ Week 2/Day 3/Core/faker_api/server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const { faker } = require('@faker-js/faker');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 
-// Function to create a new User object
 const createUser = () => {
     return {
-        _id: faker.string.uuid(),
+        _id: faker.datatype.uuid(),
         password: faker.internet.password(),
         email: faker.internet.email(),
         phoneNumber: faker.phone.imei(),        
@@ -19,10 +18,9 @@ const createUser = () => {
 
 
 
-// Function to create a new Company object
 const createCompany = () => {
     return {
-        _id: faker.string.uuid()(),
+        _id: faker.datatype.uuid(),
         name: faker.company.name(),
         address: faker.location.streetAddress(),
         street: faker.location.streetName(),
@@ -33,26 +31,24 @@ const createCompany = () => {
     };
 };
 
-// API route to create a new user
+
+
 app.get('/api/users/new', (req, res) => {
     const newUser = createUser();
     res.json(newUser);
 });
 
-// API route to create a new company
 app.get('/api/companies/new', (req, res) => {
     const newCompany = createCompany();
     res.json(newCompany);
 });
 
-// API route to create a new user and a new company
 app.get('/api/user/company', (req, res) => {
     const newUser = createUser();
     const newCompany = createCompany();
     res.json({ user: newUser, company: newCompany });
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server running on PORT : ${port}`);
 });
